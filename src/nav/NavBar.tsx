0c4fe0551e7b415d8a9a448c5e95d5db00039dee
Paul Johns <johnspaulm@gmail.com>
2024-02-23 16:54:54 -0600

    updated navbar

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -1,24 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const node = useRef();
+
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click 
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-slate-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          <NavLink to="/">coINventory</NavLink>
+    <nav className="flex items-center justify-between bg-slate-500 p-6">
+      <div className="flex items-center text-white font-semibold text-xl tracking-tight space-x-4 relative" ref={node}>
+          <NavLink to="/" onClick={() => setIsOpen(false)}>coINventory</NavLink>
           <div className="dropdown">
-            <button onClick={() => setIsOpen(!isOpen)}>
+            <p onClick={() => setIsOpen(!isOpen)} className="cursor-pointer">
               Inventory
-            </button>
+            </p>
             {isOpen && (
-              <div className="dropdown-content">
-                <NavLink to="/coins/add">Add Coin</NavLink>
+              <div className="flex flex-col w-full dropdown-content absolute bg-slate-500 rounded-lg p-2">
+                <NavLink to="/coins/add" onClick={() => setIsOpen(false)}>Add Coin</NavLink>
               </div>
             )}
           </div>
-        </span>
       </div>
     </nav>
   );

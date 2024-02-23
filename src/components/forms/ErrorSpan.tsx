a96a0e4428d7a6c5de1e25c57f5ddc2a9c42f7a2
Paul Johns <johnspaulm@gmail.com>
2024-02-23 16:56:58 -0600

    added typescript interface

--- src/components/forms/ErrorSpan.tsx
+++ src/components/forms/ErrorSpan.tsx
@@ -1,4 +1,8 @@
-const ErrorSpan = ({ title }) => {
+interface ErrorSpanProps {
+  title: string;
+}
+
+const ErrorSpan: React.FC<ErrorSpanProps> = ({ title }) => {
   return (
     <span className="bg-red-500/75 text-white p-2 m-2 rounded">
       {title} is required
